c6f558317261761783ebcbeb420d9d4a4a247d7c
1909490139 <55137659+1909490139@users.noreply.github.com>
2019-09-28 15:01:14 +0800

    333

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-chown -R postgres:postgres /var/
+chown -R postgres:postgres /var/data/
 file_env() {
         local var="$1"
         local fileVar="${var}_FILE"
