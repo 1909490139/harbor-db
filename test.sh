e8d00b6bae1a915a059fa4a5ede9cddfe2dfa9e0
1909490139 <55137659+1909490139@users.noreply.github.com>
2019-10-15 21:20:42 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -f /var/lib/postgresql/yes.txt ];do
-  /docker-healthcheck.sh /docker-entrypoint.sh
-  touch /var/lib/postgresql/yes.txt
+if [ -f /var/lib/postgresql/yes.txt ];then
+  tail -f /var/lib/postgresql/yes.txt;
 else
-  tail -f /var/lib/postgresql/yes.txt
-done
+  /docker-healthcheck.sh /docker-entrypoint.sh;
+  touch /var/lib/postgresql/yes.txt;
+fi
